a2fd913d0033eaf04d54799dfb12a8cc5ac6bf50
Fabien Geyer <fabien.geyer@tum.de>
2024-10-07 16:23:17 +0200

    Add Github workflow

---
+++ codegencmd_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"testing"
+)
+
+func TestCodegenCmd(t *testing.T) {
+	args := Args{
+		OutputDir:      "",
+		OutputCCode:    "pyexports.c",
+		OutputChdrCode: "pyexports.h",
+		OutputGoCode:   "pyexports.go",
+		PyModuleName:   "gomodule",
+		GoTags:         []string{"python"},
+	}
+
+	DoPyExports(args, []string{"testfile.go"})
+
+	cmd := exec.Command("go", "build", "-buildmode=c-shared",
+		fmt.Sprintf("--tags=%s", args.GoTags[0]), "-o", "gomodule.so")
+	t.Log("Building with:", cmd)
+	cmdout, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("Compilation error: %v. Output: %s", err, cmdout)
+	}
+}
